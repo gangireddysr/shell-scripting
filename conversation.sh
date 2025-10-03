a2fba996a979de627baa55a703ebe2f4b9b845ef
gangireddysr <gangireddy.sr96@gmail.com>
2025-10-03 12:23:57 +0530

    conversation

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#Conversation between two Persons.
+echo "Person 1 : hey! do you practice shell scripting?"
+echo "Person 2 : Yes bro. What about you?"
+echo "Person 1 : I'm also completed bro."
+echo "Person 2 : Super we can start Practice another concept bro"
+echo "Person 1 : Yeah bro"
