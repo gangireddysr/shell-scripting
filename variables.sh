2dab6f56adc424e90a9dbcf4a00c13aa882bfa17
gangireddysr <gangireddy.sr96@gmail.com>
2025-10-03 12:43:58 +0530

    message

---
+++ variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#variables no speace before or after equal
+PERSON1=Sridhar
+PERSON2=Suresh
+
+echo "$PERSON1: : hey $PERSON2! do you practice shell scripting?"
+echo "$PERSON2: : Yes $PERSON1. What about you?"
+echo "$PERSON1: : I'm also completed $PERSON2."
+echo "$PERSON2: : Super, we can start Practice another concept $PERSON1"
+echo "$PERSON1: : Yeah $PERSON2"
